e11c3672e3598f8be78699faa905a4da1f04a001
pascuC <pascu97c@gmail.com>
2020-08-26 15:39:02 -0700

    second commit

--- index.php
+++ index.php
@@ -1,8 +1,50 @@
+<?php
+$SNIEPs1 = $SNIEPs2 = $SNIEPs3 = $RPLs1 = $RPLs2 = $RENFECs1 = $RENFECs1 = $EPASs1 = $EPASs2 = $EPASs3 = $SCOM1 = $SCOM2 = "";
+$Clima1 = $Clima2 = $Clima3 = "";
+$Bandoteca = $Magazie = $CCentrala = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $SNIEPs1 = test_input($_POST['temp']);
+    echo $SNIEPs1;
+    // $SNIEPs2 = test_input($_POST['']);
+    // $SNIEPs3 = test_input($_POST['']);
+
+    // $RPLs1 = test_input($_POST['']);
+    // $RPLs2 = test_input($_POST['']);
+    // $RPLs3 = test_input($_POST['']);
+
+    // $RENFECs1 = test_input($_POST(['']));
+    // $RENFECs2 = test_input($_POST(['']));
+
+    // $EPASs1 = test_input($_POST['']);
+    // $EPASs2 = test_input($_POST['']);
+    // $EPASs3 = test_input($_POST['']);
+
+    // $SCOM1 = test_input($_POST['']);
+    // $SCOM2 = test_input($_POST['']);
+
+    // $Clima1 = test_input($_POST['']);
+    // $Clima2 = test_input($_POST['']);
+    // $Clima3 = test_input($_POST['']);
+
+    // $Bandoteca = test_input($_POST['']);
+    // $Magazie = test_input($_POST['']);
+    // $CCentrala = test_input($_POST['']);
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
         <title>DEPABD-Live monitoring</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,6 +62,7 @@
 
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
         <script src="js/icons.js"></script>
+        
     </head>
 
 
@@ -87,7 +130,7 @@
                                 <h4> SNIEP</h4>
                                 <i class="fad fa-server" style="font-size:25px;"></i>
                                 <div class="line-dec"></div>
-                                <p>S1 temperature: °C</p>
+                                <p>S1 temperature: <?php echo $SNIEPs1 ?> °C</p>
                                 <p>S2 temperature: °C</p>
                                 <p>S3 temperature: °C</p>
                             </div>
--- index_post.php
+++ index_post.php
@@ -1,33 +1,36 @@
+<?php
+
 $SNIEPs1 = $SNIEPs2 = $SNIEPs3 = $RPLs1 = $RPLs2 = $RENFECs1 = $RENFECs1 = $EPASs1 = $EPASs2 = $EPASs3 = $SCOM1 = $SCOM2 = "";
 $Clima1 = $Clima2 = $Clima3 = "";
 $Bandoteca = $Magazie = $CCentrala = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $SNIEPs1 = test_input($_POST['']);
-    $SNIEPs2 = test_input($_POST['']);
-    $SNIEPs3 = test_input($_POST['']);
+    $SNIEPs1 = test_input($_POST['temp']);
+    echo $SNIEPs1;
+    // $SNIEPs2 = test_input($_POST['']);
+    // $SNIEPs3 = test_input($_POST['']);
 
-    $RPLs1 = test_input($_POST['']);
-    $RPLs2 = test_input($_POST['']);
-    $RPLs3 = test_input($_POST['']);
+    // $RPLs1 = test_input($_POST['']);
+    // $RPLs2 = test_input($_POST['']);
+    // $RPLs3 = test_input($_POST['']);
 
-    $RENFECs1 = test_input($_POST(['']));
-    $RENFECs2 = test_input($_POST(['']));
+    // $RENFECs1 = test_input($_POST(['']));
+    // $RENFECs2 = test_input($_POST(['']));
 
-    $EPASs1 = test_input($_POST['']);
-    $EPASs2 = test_input($_POST['']);
-    $EPASs3 = test_input($_POST['']);
+    // $EPASs1 = test_input($_POST['']);
+    // $EPASs2 = test_input($_POST['']);
+    // $EPASs3 = test_input($_POST['']);
 
-    $SCOM1 = test_input($_POST['']);
-    $SCOM2 = test_input($_POST['']);
+    // $SCOM1 = test_input($_POST['']);
+    // $SCOM2 = test_input($_POST['']);
 
-    $Clima1 = test_input($_POST['']);
-    $Clima2 = test_input($_POST['']);
-    $Clima3 = test_input($_POST['']);
+    // $Clima1 = test_input($_POST['']);
+    // $Clima2 = test_input($_POST['']);
+    // $Clima3 = test_input($_POST['']);
 
-    $Bandoteca = test_input($_POST['']);
-    $Magazie = test_input($_POST['']);
-    $CCentrala = test_input($_POST['']);
+    // $Bandoteca = test_input($_POST['']);
+    // $Magazie = test_input($_POST['']);
+    // $CCentrala = test_input($_POST['']);
 }
 
 function test_input($data) {
@@ -36,3 +39,4 @@ function test_input($data) {
     $data = htmlspecialchars($data);
     return $data;
 }
+?>
